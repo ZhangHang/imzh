80a91fa9c9ac3f3ab4789b6727c799df97ff130b
ZhangHang <zh199111@gmail.com>
2015-11-12 17:35:05 +0800

    Let there be a DarkThemeMode object

--- client/index.js
+++ client/index.js
@@ -1,1 +1,38 @@
 Meteor.subscribe('user_status_sessions')
+
+DarkThemeMode = (() => {
+  let mode = {}
+  let excludeSelectorList = [".leave-alone-in-dark-mode"]
+
+  let IS_DARK_MODE_KEY = "isDarkMode"
+  Session.setDefault(IS_DARK_MODE_KEY, false)
+
+  mode.exclude = (selector) => {
+    console.assert(typeof selector === "string")
+    excludeSelectorList.push(selector)
+  }
+
+  mode.on = () => {
+    Session.set(IS_DARK_MODE_KEY, true)
+    update()
+  }
+
+  mode.off = () => {
+    Session.set(IS_DARK_MODE_KEY, false)
+    update()
+  }
+
+  mode.isOn = () => {
+    return Session.get(IS_DARK_MODE_KEY)
+  }
+
+  function update() {
+    const colorInvertCSSRule = `invert(${mode.isOn() ? 100 : 0}%)`
+    $("body").css("-webkit-filter", colorInvertCSSRule)
+    excludeSelectorList.forEach( selector => {
+      $(selector).css("-webkit-filter", colorInvertCSSRule)
+    })
+  }
+
+  return mode
+})()
--- client/templates/nav.js
+++ client/templates/nav.js
@@ -1,35 +1,15 @@
-let IS_DARK_MODE_KEY = "isDarkMode"
-Session.setDefault(IS_DARK_MODE_KEY, false)
-
 Template.nav.helpers({
-  isDarkMode(){
-    return Session.get(IS_DARK_MODE_KEY)
+  isDarkMode() {
+    return DarkThemeMode.isOn()
   }
 });
 
 Template.nav.events({
-  'click #dark-mode-switch'(event, template){
-    let isDarkMode = !Session.get(IS_DARK_MODE_KEY)
-    Session.set(IS_DARK_MODE_KEY, isDarkMode)
-
-    let body = document.body
-    const colorInvertCSSRule = `invert(${isDarkMode ? 100 : 0}%)`
-    setMultiVendorProp(body.style, "filter", colorInvertCSSRule)
-
-    let itemsToLeaveAlone = document.getElementsByClassName("leave-alone-in-dark-mode")
-    for (let i = 0; i < itemsToLeaveAlone.length; i++) {
-      setMultiVendorProp(itemsToLeaveAlone[i].style, "filter", colorInvertCSSRule)
+  'click #dark-mode-switch' (event, template) {
+    if (DarkThemeMode.isOn()) {
+      DarkThemeMode.off()
+    } else {
+      DarkThemeMode.on()
     }
   }
 });
-
-function setMultiVendorProp(style, propName, value) {
-  style[propName] = value
-
-  style[`-webkit-${propName}`] = value
-  style[`-moz-${propName}`] = value
-  style[`-o-${propName}`] = value
-  style[`-ms-${propName}`] = value
-
-  return value
-}
